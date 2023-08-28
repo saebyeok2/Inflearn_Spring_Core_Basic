38f3017cd5faf76d3f629c8baa75c44b20e9d2e3
saebyeok2 <tomato8269@naver.com>
2023-08-28 17:27:08 +0900

    test: MemberService.findMember() 테스트 구현

---
+++ src/main/java/project/core/MemberApp.java
@@ -0,0 +1,18 @@
+package project.core;
+
+import project.core.member.Grade;
+import project.core.member.Member;
+import project.core.member.MemberService;
+import project.core.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+    }
+}
--- src/main/java/project/core/member/Grade.java
+++ src/main/java/project/core/member/Grade.java
@@ -1,5 +1,6 @@
 package project.core.member;
 
 public enum Grade {
-
+    BASIC,
+    VIP
 }
