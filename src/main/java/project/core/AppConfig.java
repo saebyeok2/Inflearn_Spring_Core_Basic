3e32974ac2d3a96556351f3da77343b97d4c203e
saebyeok2 <tomato8269@naver.com>
2023-08-29 23:59:16 +0900

    refactor: AppConfig 리팩터링

--- src/main/java/project/core/AppConfig.java
+++ src/main/java/project/core/AppConfig.java
@@ -1,5 +1,6 @@
 package project.core;
 
+import project.core.discount.DiscountPolicy;
 import project.core.discount.FixDiscountPolicy;
 import project.core.member.MemberService;
 import project.core.member.MemberServiceImpl;
@@ -10,10 +11,18 @@ import project.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
