190b221930d92f0ef44ec2739a201b01e7d8a376
saebyeok2 <tomato8269@naver.com>
2023-09-14 17:19:28 +0900

    커스텀 어노테이션 실습

--- src/main/java/project/core/discount/RateDiscountPolicy.java
+++ src/main/java/project/core/discount/RateDiscountPolicy.java
@@ -1,10 +1,12 @@
 package project.core.discount;
 
 import org.springframework.stereotype.Component;
+import project.core.annotation.MainDiscountPolicy;
 import project.core.member.Grade;
 import project.core.member.Member;
 
 @Component
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy {
     private int discountPercent = 10; //10% 할인 @Override
     public int discount(Member member, int price) {
