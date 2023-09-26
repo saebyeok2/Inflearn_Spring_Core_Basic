ef3f58ee84a258ed8b1a8bd79052bb1b1a6776d2
saebyeok2 <tomato8269@naver.com>
2023-09-26 15:18:48 +0900

    request 스코프 실습

---
+++ src/main/java/project/core/web/LogDemoService.java
@@ -0,0 +1,17 @@
+package project.core.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+import project.core.common.MyLogger;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
+}
